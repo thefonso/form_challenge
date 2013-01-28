350e8561e0dbbb09eff4ee5531e31894ed2dcdb5
thefonso <alephmage@gmail.com>
2013-01-28 16:58:10 -0600

    works but want to make it better

--- app/middleware/form_validator.rb
+++ app/middleware/form_validator.rb
@@ -5,33 +5,32 @@ class FormValidator
    @app = app
  end
  
- def call(env)  
-   # if request_path = "form post" then do stuff...
+ def call(env)
    request = Rack::Request.new(env)
-   # response = Rack::Response.new(["I HATE YOU WORLD"],200)
 
-   
    # return request
    # pp request # whole object
    # pp request.env["rack.request.form_hash"]
-   # pp request.env.fetch("rack.request.form_hash", {}).fetch("post", {})["name"]
-   
-
+    pp "HELLO !!!!!!!!!!!!!!!!!!!"
+      age_value = request.env.fetch("rack.request.form_hash", {}).fetch("post", {})["age"]
+      pp age_value
+      pp /\d+/.match(age_value)
+   # if the form is not empty
    if request.env["rack.request.form_hash"] != nil
-     
-     # look for specifc params field
-     pp request.env.fetch("rack.request.form_hash", {}).fetch("post", {})["name"]
 
-     # do stuff for security check
-     
-     
+     # if age field is not empty
+     if /\d+/.match(age_value) == nil
+        age_value = "enter your age"
+     else
+
+     end
      # then pass on to rails app
      @app.call(env)
+
    else
      p "nil indicator here !!!!!!!"
      @app.call(env)
    end
-   
+
  end
- 
 end
