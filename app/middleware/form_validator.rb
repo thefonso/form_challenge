6864f2806a0f1ec182a12deda25e8eebd82868e7
thefonso <alephmage@gmail.com>
2013-01-29 12:47:36 -0600

    working on rest of form elements ....story3

--- app/middleware/form_validator.rb
+++ app/middleware/form_validator.rb
@@ -5,31 +5,30 @@ class FormValidator
    @app = app
  end
 
- def not_found
-   raise ActionController::RoutingError.new('Forgot to enter your age....hit the browser back button')
- end
-
  def call(env)
    request = Rack::Request.new(env)
 
    # return request
    # pp request # whole object
    # pp request.env["rack.request.form_hash"]
-    pp "HELLO !!!!!!!!!!!!!!!!!!!"
       age_value = request.env.fetch("rack.request.form_hash", {}).fetch("post", {})["age"]
-      pp age_value
-      pp /\d+/.match(age_value)
-   # if the form is not empty
+      name_value = request.env.fetch("rack.request.form_hash", {}).fetch("post", {})["name"]
+      title_value = request.env.fetch("rack.request.form_hash", {}).fetch("post", {})["title"]
+      content_value = request.env.fetch("rack.request.form_hash", {}).fetch("post", {})["content"]
+      # if the form is not empty
    if request.env["rack.request.form_hash"] != nil
 
-     # if age field is not empty
-     if /\d+/.match(age_value) == nil
-        #redirect to 404 error
-       not_found
-     else
-
+    # if name is empty send error
+     if name_value == nil
+      raise ActionController::RoutingError.new('Forgot to enter your name....hit the browser back button')
+     elsif title_value == nil
+      raise ActionController::RoutingError.new('Enter your title....hit the browser back button')
+     elsif content_value == nil
+      raise ActionController::RoutingError.new('Please enter some content....hit the browser back button')
+     elsif /\d+/.match(age_value) == nil
+       raise ActionController::RoutingError.new('Forgot to enter your age....hit the browser back button')
      end
-     # then pass on to rails app
+
      @app.call(env)
 
    else
