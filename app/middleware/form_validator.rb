4ccfaa517feb1f88b9aa13e2afa4c99c5c29eec1
thefonso <alephmage@gmail.com>
2013-01-29 14:06:36 -0600

    story three done

--- app/middleware/form_validator.rb
+++ app/middleware/form_validator.rb
@@ -15,15 +15,21 @@ class FormValidator
       name_value = request.env.fetch("rack.request.form_hash", {}).fetch("post", {})["name"]
       title_value = request.env.fetch("rack.request.form_hash", {}).fetch("post", {})["title"]
       content_value = request.env.fetch("rack.request.form_hash", {}).fetch("post", {})["content"]
+      pp "LOOK AT ME !!!!!!!!!!!!!!!!!"
+      pp name_value
+      pp title_value
+      pp content_value
+      pp age_value
+
       # if the form is not empty
    if request.env["rack.request.form_hash"] != nil
 
     # if name is empty send error
-     if name_value == nil
+     if name_value == ""
       raise ActionController::RoutingError.new('Forgot to enter your name....hit the browser back button')
-     elsif title_value == nil
+     elsif title_value == ""
       raise ActionController::RoutingError.new('Enter your title....hit the browser back button')
-     elsif content_value == nil
+     elsif content_value == ""
       raise ActionController::RoutingError.new('Please enter some content....hit the browser back button')
      elsif /\d+/.match(age_value) == nil
        raise ActionController::RoutingError.new('Forgot to enter your age....hit the browser back button')
