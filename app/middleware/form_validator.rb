80aed3489f014c7a525c634db34e32c2f6c1ee3f
thefonso <alephmage@gmail.com>
2013-01-31 15:57:16 -0600

    ready to get rspec workin

--- app/middleware/form_validator.rb
+++ app/middleware/form_validator.rb
@@ -1,40 +1,46 @@
 require 'pp'
 
 class FormValidator
-  def initialize(app)
-    @app = app
-  end
-  def call(env) # Rack only needs one method 'call'
-    request = Rack::Request.new(env)
+ def initialize(app)
+   @app = app
+ end
 
-    items = ["age"]
-    items.each do |field_name|
-      # how to abstract this?
-      formbox_value = request.env.fetch("rack.request.form_hash", {}).fetch("post", {})[field_name]
+ def call(env)
+   request = Rack::Request.new(env)
 
-      # TODO How am I calling the YML file?
-
+   # return request
+   # pp request # whole object
+   # pp request.env["rack.request.form_hash"]
+      age_value = request.env.fetch("rack.request.form_hash", {}).fetch("post", {})["age"]
+      name_value = request.env.fetch("rack.request.form_hash", {}).fetch("post", {})["name"]
+      title_value = request.env.fetch("rack.request.form_hash", {}).fetch("post", {})["title"]
+      content_value = request.env.fetch("rack.request.form_hash", {}).fetch("post", {})["content"]
       pp "LOOK AT ME !!!!!!!!!!!!!!!!!"
-      pp formbox_value
+      pp name_value
+      pp title_value
+      pp content_value
+      pp age_value
 
-      # TODO abstract these things out so that you can create a hash and then serializ the Hash.
-      # You want to eventually pull all this out and offer it as middleware anyone can use.
-      # part 1 - kill duplication
-      # part 2 - pull things out into yml file
-      if request.env["rack.request.form_hash"] != nil
+      # if the form is not empty
+   if request.env["rack.request.form_hash"] != nil
 
-        # if field is empty send error
-        if formbox_value == ""
-          raise ActionController::RoutingError.new('Forgot to enter your #{field_name}....hit the browser back button')
-        end
+    # if name is empty send error
+     if name_value == ""
+      raise ActionController::RoutingError.new('Forgot to enter your name....hit the browser back button')
+     elsif title_value == ""
+      raise ActionController::RoutingError.new('Enter your title....hit the browser back button')
+     elsif content_value == ""
+      raise ActionController::RoutingError.new('Please enter some content....hit the browser back button')
+     elsif /\d+/.match(age_value) == nil
+       raise ActionController::RoutingError.new('Forgot to enter your age....hit the browser back button')
+     end
 
-        @app.call(env)
+     @app.call(env)
 
-      else
-        p "nil indicator here !!!!!!!"
-        @app.call(env)
-      end
-    end
-  end
+   else
+     p "nil indicator here !!!!!!!"
+     @app.call(env)
+   end
 
+ end
 end
