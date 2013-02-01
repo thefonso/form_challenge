23ccd2567f112e2aaa37e4b80ec633386109a02e
thefonso <alephmage@gmail.com>
2013-01-31 18:11:55 -0600

    investigating rspec

--- spec/middleware/form_validator_spec.rb
+++ spec/middleware/form_validator_spec.rb
@@ -1,12 +1,26 @@
+require_relative '../spec_helper'
 require_relative '../../app/middleware/form_validator'
 
 
 
 describe FormValidator do
-  let (:formvalidator) {FormValidator.new}
-  it 'should exist' do
+  let(:env) {Rack::MockRequest.env_for('/')}
+  let(:formvalidator) {FormValidator.new}
+
+  def request!(other_env)
+    @status, @headers, @response = Rails.application.call(env.merge(other_env))
+  end
+
+  xit 'should exist' do
     # TODO how to mock this .call method? Rack::MockRequest?
-    form = formvalidator.call()
+    #
+    fake_env = 
+    form = formvalidator.call(fake_env)
     form.should == nil
+  end
+  it "redirects to address sans www" do
+    request!("HTTP_HOST" => "www.example.com")
+    @status.should eql(301)
+    @headers["Location"].should eql("http://example.com")
   end
 end
